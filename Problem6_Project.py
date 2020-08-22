016543ab029adc60906b4e91c3ef00b15a4c968c
Antonson <antonson555@gmail.com>
2020-08-22 18:12:11 +0530

    Added Problem6 Project question

---
+++ Problem6_Project.py
@@ -0,0 +1,6 @@
+'''
+Q6
+https://en.wikipedia.org/wiki/Birthday_problem
+Design a program that can test the Birthday problem, by a series of experiments, on randomly generated
+birthdays which test this paradox for n = 5,10,15,20,25,30...200.
+'''
