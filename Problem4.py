c86292c757bc40b16981e193a331ca360861b4e1
Antonson <antonson555@gmail.com>
2020-08-22 17:34:11 +0530

    Problem4 solution update using lower()

--- Problem4.py
+++ Problem4.py
@@ -1,8 +1,9 @@
 def findvowel(a):
+    a = a.lower()
     arr= [char for char in a]
     count=0
     for i in a:
-        if i=='a' or i=='e' or i=='i' or i=='o' or i=='u' or i=='A' or i=='E' or i=='I' or i=='O' or i=='U':
+        if i=='a' or i=='e' or i=='i' or i=='o' or i=='u':
             count+=1
     return count
 
