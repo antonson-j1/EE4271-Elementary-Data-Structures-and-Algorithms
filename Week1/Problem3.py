ef7fcc186d0efcc8cee02625898003bac2f0e1e2
Antonson J <60357885+antonson-j1@users.noreply.github.com>
2020-08-23 19:53:28 +0530

    Problem3 updated

--- Week1/Problem3.py
+++ Week1/Problem3.py
@@ -1,8 +1,8 @@
 a = list(map(int,input().split()))
 
 def ocheck(a):
-    for i in range(len(a)):
-        for j in range(len(a)):
+    for i in len(a):
+        for j in len(a):
             if i!=j:
                 product = a[i]*a[j]
                 if product%2!=0:
